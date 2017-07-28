de681cabc218cac4433d89662e36e14b3dca30b9
gitnavV <navneetdas53@gmail.com>
2017-07-28 17:16:49 +0530

    Removed All Files & Started Fresh

---
+++ app/src/main/java/shuttlr/gitwebx/github/io/shuttlr/Map.java
@@ -0,0 +1,13 @@
+package shuttlr.gitwebx.github.io.shuttlr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Map extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
